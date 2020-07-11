17c32654e51370c00e82d7e98e713d5be62311b2
keshav021 <keshavag007@gmail.com>
2020-07-11 20:09:19 +0530

    Added func to calculate gear inches and added an additional constructr

--- Gear.java
+++ Gear.java
@@ -5,23 +5,36 @@ import java.util.*;
 class Gear{
 	private int chainring;
 	private int cog;
-	private float ratio;
+	private int rim;
+	private double tire;
+	private double ratio;
 
 	Gear(int chainring, int cog){
 		this.chainring = chainring;
 		this.cog = cog;
 	}
 
-	float calculateRatio(){
-		ratio = ((float)chainring)/cog;
+	Gear(int chainring, int cog, int rim, double tire){
+		this(chainring, cog);
+		this.rim = rim;
+		this.tire = tire;
+	}
+
+	double calculateRatio(){
+		ratio = ((double)chainring)/cog;
 		return ratio;
 	}
 
+	double calculateGearInches(){
+		return ratio*(rim + tire * 2);
+	}
+
 	public static void main(String args[]){
-		Gear g1=  new Gear(52, 11);
+		Gear g1=  new Gear(52, 11, 24, 1.5);
 		Gear g2 = new Gear(30, 27);
 		System.out.println(g1.calculateRatio());
 		System.out.println(g2.calculateRatio());
+		System.out.println(g1.calculateGearInches());
 	}
 
 }
